d634ad05fe50dc199d5c907921f7ddbb7237e448
RostiMelk <hello@rosti.no>
2021-08-09 19:56:30 +0200

    Compile to commonjs

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,23 +8,59 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import fs from 'fs';
-const extractSvgToFontVars = (fileInput, fileOutput) => __awaiter(void 0, void 0, void 0, function* () {
-    const fileData = fs.readFileSync(fileInput, 'utf8');
-    const allLines = fileData.split('\n');
-    const varsArray = [];
-    for (const line of allLines) {
-        if (line.startsWith('$')) {
-            varsArray.push(line);
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var extractSvgToFontVars = function (fileInput, fileOutput) { return __awaiter(void 0, void 0, void 0, function () {
+    var fileData, allLines, varsArray, _i, allLines_1, line, varsString;
+    return __generator(this, function (_a) {
+        fileData = fs_1.default.readFileSync(fileInput, 'utf8');
+        allLines = fileData.split('\n');
+        varsArray = [];
+        for (_i = 0, allLines_1 = allLines; _i < allLines_1.length; _i++) {
+            line = allLines_1[_i];
+            if (line.startsWith('$')) {
+                varsArray.push(line);
+            }
         }
-    }
-    const varsString = varsArray.join('\n');
-    try {
-        fs.writeFileSync(fileOutput, varsString);
-    }
-    catch (err) {
-        console.error(err);
-    }
-});
-export default extractSvgToFontVars;
+        varsString = varsArray.join('\n');
+        try {
+            fs_1.default.writeFileSync(fileOutput, varsString);
+        }
+        catch (err) {
+            console.error(err);
+        }
+        return [2 /*return*/];
+    });
+}); };
+exports.default = extractSvgToFontVars;
 //# sourceMappingURL=index.js.map
